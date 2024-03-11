943bef4550646597c8753bb3bc0da241627a568c
Genia Melnik <geniasheiko@gmail.com>
2024-03-11 13:08:32 +0200

    JS finished. Fixed HTML

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
+const translate1 = document.getElementById("translate1");
+const translate2 = document.getElementById("translate2");
+const translate3 = document.getElementById("translate3");
+const learnWord1 = document.getElementById("words-rus1");
+const learnWord2 = document.getElementById("words-rus2");
+const learnWord3 = document.getElementById("words-rus3");
+const wrapper = document.getElementById("wrapper");
+
 //---------Create object
 function addElementToObject() {
    var words = {};
    var word1 = document.getElementById("word1");
-   var translate1 = document.getElementById("translate1");
    var subscribe1 = word1.value;
    var subscribe2 = translate1.value;
 
    var word2 = document.getElementById("word2");
-   var translate2 = document.getElementById("translate2");
    var subscribe3 = word2.value;
    var subscribe4 = translate2.value;
 
    var word3 = document.getElementById("word3");
-   var translate3 = document.getElementById("translate3");
    var subscribe5 = word3.value;
    var subscribe6 = translate3.value;
 
@@ -25,24 +30,58 @@ function addElementToObject() {
 };
 
 //--------- Add EventListener
-var element = document.getElementById('click-me');
-element.addEventListener('click', () => {
-   var allWords = addElementToObject();
-   //--------- Create array include all keys
-   var wordsKeys = Object.keys(allWords);
 
+const buttonClick = document.getElementById('click-me');
+const buttonCheck = document.getElementById('button-check');
+var allWords, wordsKeys, wordsValues;
+buttonClick.addEventListener('click', () => {
+   allWords = addElementToObject();
+   wordsKeys = Object.keys(allWords);
+   wordsValues = Object.values(allWords);
    //--------- Change element
-   var learnWord1 = document.getElementById("words-rus1");
    learnWord1.innerHTML = "<p>" + wordsKeys[0] + "</p>";
-   var learnWord2 = document.getElementById("words-rus2");
    learnWord2.innerHTML = "<p>" + wordsKeys[1] + "</p>";
-   var learnWord3 = document.getElementById("words-rus3");
    learnWord3.innerHTML = "<p>" + wordsKeys[2] + "</p>";
+
+   translate1.value = "";
+   translate2.value = "";
+   translate3.value = "";
 
    var outputWord1 = document.getElementById("words-deutsch1");
    var outputWord2 = document.getElementById("words-deutsch2");
    var outputWord3 = document.getElementById("words-deutsch3");
 
+   buttonClick.style.display = "none";
+   buttonCheck.style.display = "block";
+
+});
+
+buttonCheck.addEventListener('click', () => {
+
+   var result = 0;
+   if (wordsValues[0] === translate1.value) {
+      result++;
+   }
+   if (wordsValues[1] === translate2.value) {
+      result++;
+   }
+   if (wordsValues[2] === translate3.value) {
+      result++;
+   }
+
+   if (result == 0) {
+      wrapper.innerHTML = "<p>Ужас! Надо хорошо учиться. Вы неугадали ни одного слова:(.</p>";
+   };
+
+   if (result == 1) {
+      wrapper.innerHTML = "<p>Плохо:( Вы угадали <span>" + result + "</span> словo.</p>";
+   };
+   if (result == 2) {
+      wrapper.innerHTML = "<p>Не плохо! Вы угадали <span>" + result + "</span> слова.</p>";
+   };
+   if (result == 3) {
+      wrapper.innerHTML = "<p>Отлично! Молодец!!!Вы угадали <span>" + result + "</span> слова.</p>";
+   }
 });
 
 
